3d9c4a110aff5aee6e9b3c352f0617cbef988de1
cv5587 <64960648+cv5587@users.noreply.github.com>
2024-09-19 16:38:24 +0900

    Apply .gitignore

--- CodeTest1/boj1546.cpp
+++ CodeTest1/boj1546.cpp
@@ -1,9 +1,37 @@
-﻿//평균
-#include <iostream>
-#include <vector>
-#include <string>
+﻿//평균 1546
+#include<bits/stdc++.h>
 using namespace std;
 
+#pragma region 해설
+int main()
+{
+	int N = 0;
+	int A[1000];
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> A[i];
+	}
+
+	long sum = 0;
+	long max = 0;
+
+	for (int i= 0; i < N; i++)
+	{
+		if (A[i] > max) {
+			max = A[i];
+		}
+		sum = sum + A[i];
+	}
+	double result = sum * 100.0 / max / N;
+	cout << result;
+}
+#pragma endregion
+
+
+#pragma region 내가푼거
+/*
 int main()
 {
 	int N, Arr[1000] = { 0 };
@@ -57,3 +85,5 @@ int main()
 	fResult = (SumScore + 100.f) / (float)N;
 	std::cout << fResult;
 }
+*/
+#pragma endregion
