6d09d3817d845c3acd8ba13f0ac80ece9a802423
cv5587 <64960648+cv5587@users.noreply.github.com>
2024-09-25 22:50:07 +0900

    1103

---
+++ CodeTest1/boj11003.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int N, L;
+
+	cin >> N >> L;
+
+	string A;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> A[i];
+	}
+
+	int S = 0, E = 0;
+
+
+
+
+}
