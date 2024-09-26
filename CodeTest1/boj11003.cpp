3af7cf792f42c8c6985f5933fb9bda95fc5c9842
cv5587 <64960648+cv5587@users.noreply.github.com>
2024-09-26 12:38:35 +0900

    최솟값

--- CodeTest1/boj11003.cpp
+++ CodeTest1/boj11003.cpp
@@ -2,22 +2,32 @@
 
 using namespace std;
 
+typedef pair<int, int> Node;
 int main()
 {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
 	int N, L;
-
 	cin >> N >> L;
-
-	string A;
+	deque<Node> mydeque;
 
 	for (int i = 0; i < N; i++)
 	{
-		cin >> A[i];
+		int now;
+		cin >> now;
+
+		while (mydeque.size() && mydeque.back().first>now)
+		{
+			mydeque.pop_back();
+		}
+		mydeque.push_back(Node(now, i));
+
+		if (mydeque.front().second <= i - L)
+		{
+			mydeque.pop_front();
+		}
+		cout << mydeque.front().first << ' ';
 	}
-
-	int S = 0, E = 0;
-
-
-
-
 }
