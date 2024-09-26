951cdc1e7c1d54a70a99a43fab476ae8ede61a60
cv5587 <64960648+cv5587@users.noreply.github.com>
2024-09-26 22:12:22 +0900

    스택으로 수열 만들기

--- CodeTest1/boj1874.cpp
+++ CodeTest1/boj1874.cpp
@@ -8,6 +8,32 @@ int main()
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
+	stack<int> S;
+	string Word;
+
+	int N;
+	int L=0;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int A;
+		cin >> A;
+
+	}
+
+	if (0 == S.size())
+	{
+		cout << "NO" << "\n";
+
+		return 0;
+	}
+
+	for (int i = 0; i < Word.length()-1; i++)
+	{
+		cout << Word[i] << "\n";
+	}
 
 
 }
+
